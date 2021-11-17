0a03c45955008b04fca09803a2babc240d03ce97
daniel hildebrandt <d.hildebrandt93@gmail.com>
2021-11-16 23:04:04 -0700

    minor phone version fixes

--- app/Services/GameService.js
+++ app/Services/GameService.js
@@ -28,7 +28,7 @@ class GameService {
     ProxyState.position = ProxyState.goal
     ProxyState.level += 1
     ProxyState.points += 1
-    if ($(window).width < 700) {
+    if ($(window).width() < 700) {
       ProxyState.timer += 1
     } else {
       ProxyState.timer += 5
